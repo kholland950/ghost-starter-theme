51d51a3cdf59fbb1117736a54150de35fb600fb1
kholland950 <kevinholland94@gmail.com>
2018-10-14 13:39:07 -0400

    Redesigned post list

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.post-grid article').forEach((it) => {
+    document.querySelectorAll('.post-list article .image-container').forEach((it) => {
         it.addEventListener('click', function() {
             this.querySelector('.post-link').click()
         })
+    })
+    document.getElementById('menu-toggle').addEventListener('click', function(e) {
+      e.stopPropagation()
+      var holder = document.getElementById('menu-holder')
+      if (holder.className === 'active') {
+        holder.className = ''
+      } else {
+        holder.className = 'active'
+      }
+    })
+    document.getElementById('menu-holder').addEventListener('click', function (e) {
+      e.stopPropagation()
+      var holder = document.getElementById('menu-holder')
+      holder.className = ''
+    })
+    document.getElementById('menu-drawer').addEventListener('click', function (e) {
+      e.stopPropagation()
     })
 })
